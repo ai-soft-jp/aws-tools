3546cb608ac4e250706c3d71a2df93490ecb4600
IWASE <iwase@ai-soft.co.jp>
2019-07-08 15:29:28 +0900

    expiration

--- guardduty/update_postfix_rejection.rb
+++ guardduty/update_postfix_rejection.rb
@@ -9,9 +9,11 @@ def die(*mesg, exitstatus: 1)
   $stderr.puts *mesg
   exit exitstatus
 end
+def log(mesg)
+  # No action
+end
 
 config = {
-  domainmap: nil,
   postmap: 'postmap',
 }
 
@@ -19,7 +21,9 @@ o = OptionParser.new
 o.on('-d PATH', '--domain-map', String, 'Path to domain map file') { |v| config[:domainmap] = v }
 o.on('-m PATH', '--postmap', String, 'Path to postmap program (default: search in $PATH)') { config[:postmap] = v }
 o.on('-a', '--archive', 'Archive finding automatically') { config[:archive] = true }
-o.on('-h', 'Show this help') { puts o; exit }
+o.on('-e DAYS', '--expires', Integer, 'Remove from list after specified DAYS') { |v| config[:expires] = v }
+o.on('-v', '--verbose', 'Be verbose') { config[:verbose] = true }
+o.on('-h', '--help', 'Show this help') { puts o; exit }
 begin
   o.parse!(ARGV)
 rescue => e
@@ -27,23 +31,32 @@ rescue => e
 end
 
 unless config[:domainmap]
-  die "Specify --domain-map"
+  die "Specify path to domain map file."
+end
+if config[:verbose]
+  def log(mesg)
+    $stderr.puts mesg
+  end
 end
 
 dmapf = open(config[:domainmap], File::RDWR | File::CREAT)
 dmap = {}
 while line = dmapf.gets
-  if /^\s*#/ =~ line
+  if /\A\s*#/ =~ line
     comment = line.strip
+  elsif /\A\s*\z/ =~ line
+    # Skip
   else
     domain, action = line.chomp.split(/\t+/)
     dmap[domain] = [action, comment]
     comment = nil
   end
 end
+log "#{config[:domainmap]}: Fetched #{dmap.size} domains."
 
 gd = Aws::GuardDuty::Client.new
 detector_id = gd.list_detectors.detector_ids.first
+log "GuardDuty: detectorId is #{detector_id}."
 token = ''
 archives = []
 begin
@@ -66,6 +79,7 @@ begin
   )
   findings.findings.each do |finding|
     domain = finding.service.action.dns_request_action.domain
+    log "Finding: #{finding.type} #{domain}"
     archives << finding.id if dmap[domain]
     dmap[domain] = [
       "REJECT Access denied by GuardDuty",
@@ -81,6 +95,28 @@ if config[:archive] && !archives.empty?
       finding_ids: finding_ids
     )
   end
+  log "GuardDuty: Archived #{archives.size} findings."
+end
+
+if config[:expires]
+  require 'time'
+  threshold = Time.now.to_i
+  threshold -= threshold % 86400
+  threshold -= config[:expires] * 86400
+  log "Threashold is #{Time.at(threshold)}"
+  dmap.reject! do |domain, (action, comment)|
+    if /@ (\S+)/ =~ comment
+      last_seen = $1
+      begin
+        if Time.parse(last_seen).to_i < threshold
+          log "#{domain}: Expired. Last seen at #{last_seen}."
+          true
+        end
+      rescue
+        false
+      end
+    end
+  end
 end
 
 dmapf.rewind
@@ -93,4 +129,5 @@ end
 dmapf.truncate(dmapf.pos)
 dmapf.fdatasync
 dmapf.close
+log "#{config[:domainmap]}: Written #{dmap.size} domains."
 system "#{Shellwords.escape(config[:postmap])} #{Shellwords.escape(config[:domainmap])}"
